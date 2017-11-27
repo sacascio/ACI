a7332406a74df01cf19c6c1b11d49bc4f748a28e
Cascio, Salvatore (sacascio) <sacascio@cisco.com>
2017-11-27 15:36:06 -0500

    login

--- login.py
+++ login.py
@@ -5,10 +5,13 @@ import requests
 # Created by Salvatore Cascio (sacascio)
 # May 16, 2017
 # U: MAY 19
-headers = {'Content-type': 'application/json'}
-url = 'https://192.168.29.133/api/aaaLogin.json'
 
-pay =  { "aaaUser" : { "attributes": {"name":"admin","pwd":"admin12345"} } }
+requests.packages.urllib3.disable_warnings()
+
+headers = {'Content-type': 'application/json'}
+url = 'https://10.90.60.174/api/aaaLogin.json'
+
+pay =  { "aaaUser" : { "attributes": {"name":"admin","pwd":"pr@ct1ce"} } }
 payload =  json.dumps(pay)
 
 response = requests.post(url, data=payload, headers=headers)
