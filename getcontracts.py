ac528be0c6f7695960a18670715358006209c8f8
sacascio <sacascio@cisco.com>
2020-03-31 17:40:25 -0400

    Update getcontracts.py
    converted to python3

--- getcontracts.py
+++ getcontracts.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import requests
@@ -8,18 +8,18 @@ import getopt
 import os
 
 def usage():
-    print "Usage: " +  sys.argv[0] + " -f|file <inputfile>"
-    print ""
-    print "-f|--file:   Pass input file of EPGs to check, one per line.  Format should be:"
-    print "tenant,ap,epg"
-    print "-h|--help: print help message"
+    print ("Usage: " +  sys.argv[0] + " -f|file <inputfile>")
+    print ("")
+    print ("-f|--file:   Pass input file of EPGs to check, one per line.  Format should be:")
+    print ("tenant,ap,epg")
+    print ("-h|--help: print help message")
     sys.exit(1)
 
 def main(argv):
 	try:
         	opts,args = getopt.getopt(argv,"f:",["file=","help"])
 	except getopt.GetoptError as err:
-       		print str(err)
+       		print (str(err))
         	sys.exit(2)
 	else:
 		for opt,arg in opts:
@@ -31,17 +31,16 @@ def main(argv):
 	try:
 		infile
 	except NameError:
-		print "Input file not passed (-f|--file)"
+		print ("Input file not passed (-f|--file)")
 		sys.exit(9)
 
 	if not os.path.isfile(infile):
-		print sys.argv[0] + " Input File %s NOT found" % infile
+		print (sys.argv[0] + " Input File %s NOT found" % infile)
 		sys.exit(9)
 
-
-	username = raw_input("\n\nEnter APIC username and press enter: \n\n")
+	username = input("\n\nEnter APIC username and press enter: \n\n")
 	password = getpass.getpass("\n\nEnter APIC password and press enter: \n\n")
-	apic = raw_input("\n\nEnter APIC IP and press enter: \n\n")
+	apic = input("\n\nEnter APIC IP and press enter: \n\n")
 
 	requests.packages.urllib3.disable_warnings()
 	
@@ -50,7 +49,7 @@ def main(argv):
 	response = requests.post("https://" + str(apic) + "/api/aaaLogin.json", json=headers, verify=False, timeout=10)
 
 	if response.status_code != 200:
-		print "Login failed to APIC: " + apic + ".  Please check IP and credentials."
+		print ("Login failed to APIC: " + apic + ".  Please check IP and credentials.")
 		sys.exit(9)
 
 
@@ -64,52 +63,40 @@ def main(argv):
 
 	for x in epgs:
 		xx = x.split(",")
-		tenant = xx[0]
-		ap = xx[1]
-		epgname = xx[2]
+		construct_type = xx[0]
+		tenant = xx[1]
+		ap = xx[2]
+		epgname = xx[3]
 		epgname = epgname.rstrip()
-		url="https://" + apic + "/api/node/mo/uni/tn-" + tenant + "/ap-" + ap + "/epg-" + epgname + ".json?query-target=subtree&target-subtree-class=fvRsCons&target-subtree-class=fvRsConsIf,fvRsProtBy,fvRsProv"
+
+		if construct_type == 'EPG':
+			middle_part = "/ap-" + ap + "/epg-"
+		else:
+			middle_part = "/out-" + ap + "/instP-"
+			
 		
+		url="https://" + apic + "/api/node/mo/uni/tn-" + tenant + middle_part + epgname + ".json?query-target=subtree&target-subtree-class=fvRsCons&target-subtree-class=fvRsConsIf,fvRsProtBy,fvRsProv"
+	
 		response = requests.get("%s" % (url),
                              cookies=cookie,
                              verify=False,  # disable SSH certificate verification
                              timeout=10)
 
 		if response.status_code == 200:
-			provcontract = response.json()['imdata'][0]['fvRsProv']['attributes']['tnVzBrCPName']
-			provstatus = response.json()['imdata'][0]['fvRsProv']['attributes']['state']
-			conscontract = response.json()['imdata'][1]['fvRsCons']['attributes']['tnVzBrCPName']
-			consstatus = response.json()['imdata'][1]['fvRsCons']['attributes']['state']
-		
+			for i in response.json()["imdata"]:
+				for k in i.keys():
+					contract = i[k]['attributes']['tnVzBrCPName']
+					state    = i[k]['attributes']['state']
 
-			try:
-				 provcontract
-			except NameError:
-				provcontract = 'N/A'
-			
-			try:
-				 provstatus
-			except NameError:
-				provstatus = 'N/A'
-			
-			try:
-				 conscontract
-			except NameError:
-				conscontract = 'N/A'
-			
-			try:
-				 consstatus
-			except NameError:
-				consstatus = 'N/A'
-			
-			f.write("EPG: " + epgname + ", " +  "Provider: " + provcontract + ", status: " +  provstatus + "\n")
-			f.write("EPG: " + epgname + ", " +  "Consumer: " + conscontract + ", status: " +  consstatus + "\n")
-			del provcontract
-			del provstatus
-			del conscontract
-			del consstatus
+					if k == 'fvRsProv':
+						p_or_c = 'Provider'
+					else:
+						p_or_c = 'Consumer'
+
+					f.write(construct_type + ": " + epgname + ", " +  p_or_c + ": " + contract + ", status: " + state + "\n")
+
 		else:
-			print "Failed to get Provide contract info for EPG: %s" % epgname
+			print ("Failed to get Provide contract info for EPG: %s" % epgname)
 	f.close()
 if __name__ == '__main__':
         main(sys.argv[1:])
