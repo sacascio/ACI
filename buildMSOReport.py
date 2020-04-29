27681451b684e98c5a795f1ceb7b0ff571a061ba
sacascio <sacascio@cisco.com>
2020-04-29 17:55:28 -0400

    Update buildMSOReport.py
    development continues

--- buildMSOReport.py
+++ buildMSOReport.py
@@ -1,8 +1,46 @@
 #!/usr/bin/env python3
 import sys
 import json
+import re
+import openpyxl
+from openpyxl import Workbook
+from openpyxl.utils import get_column_letter
 
+def reconcileL3out(dc,bd,tenant):
+	print ("checking " + bd)
+	dafe = 'SG_PBR/' + dc + '_GIS_DAFE.xlsx'
+	worksheets = []
+	l3out_list = []
+	wb = openpyxl.load_workbook(dafe, data_only=True)
 
+	for sheet in wb:
+		worksheets.append(sheet.title)
+	wb.close()
+	wb.active = worksheets.index('bd_l3out')
+	ws = wb.active
+    
+	row_start = ws.min_row
+	row_end   = ws.max_row
+
+	for x in range(row_start+1,row_end+1):
+		cell = 'A' + str(x)
+		bd_name = ws[cell].value
+
+		cell = 'B' + str(x)
+		tenant_name = ws[cell].value
+		
+		if bd_name == bd and tenant_name == tenant:
+			cell = 'C' + str(x)
+			l3out_name = ws[cell].value
+			l3out_list.append(l3out_name)
+
+	if len(l3out_list) > 1:
+		print ("WARNING: More than 1 L3out found for Tenant %s, BD %s" % (tenant,bd))
+	if len(l3out_list) == 0:
+		return 'N/A'
+	if len(l3out_list) == 1:
+		#print ("INFO: Found L3Out for BD %s, Tenant %s" % (bd,tenant))
+		return l3out_list[0]
 
 def loadSites(sitefile) :
 
@@ -37,13 +75,19 @@ def loadTenants(tenantfile) :
 def loadSchemas(schemafile,tenantData,siteData) :
 
 	schemaData = {}
+	found = 0
 
 	with open (schemafile) as schema:
 		data = json.load(schema)
 
 	for i in data:
+		temp_bds = []
 		schemaID =  i['_id']['$oid']
 		schemaName = i['displayName']
+		#print ("checking schema " + schemaName)
+		if bool((re.search('VRF-schema',schemaName,re.IGNORECASE))):
+			print ("INFO: SKIPPING " +  schemaName)
+			continue
 
 		if schemaID not in schemaData:
 			schemaData[schemaID] = {}
@@ -54,6 +98,14 @@ def loadSchemas(schemafile,tenantData,siteData) :
 			schemaData[schemaID][schemaName] = {}
 		else:
 			print ("WARNING: Schema name %s already found in schema ID %s" % (schemaName,schemaID))
+
+		# Get the BD names where more than 1 BD exists at the site.  This is a workaround because other templates have 0 BD at the site, which seems like a bug
+		for zz in i['sites']:
+			if len(zz['bds']) > 1:
+				siteID = zz['siteId']
+				for z in zz['bds']:
+					temp_bds.append({'siteID' : siteID, 'bdName' : z['bdRef']['bdName'], 'schemaId' : z['bdRef']['schemaId'], 'templateName' : z['bdRef']['templateName'], 'l3Out' : z['l3Outs']})
+				#print (temp_bds)
 
 		for j in i['templates']:
 			templateName = j['name']
@@ -109,82 +161,120 @@ def loadSchemas(schemafile,tenantData,siteData) :
 					if l['epgRef']['epgName'] != epgName:
 						print ("WARNING: EPG name %s in EPGREF is not the same as EPG Name found previously" % (epgName))
 				
-			for k in j['bds']:
-				bdName = k['name']
+			for m in j['bds']:
+				bdName = m['name']
 				schemaData[schemaID][schemaName][templateName]['bd'].append(bdName)
 
-				if k['bdRef']['schemaId'] != schemaID:
+				if m['bdRef']['schemaId'] != schemaID:
 					print ("WARNING: Schema ID for schema %s in BDREF of template %s not the same as schemaID found previously" % (schemaName,templateName))
 
-				if k['bdRef']['templateName'] != templateName:
+				if m['bdRef']['templateName'] != templateName:
 					print ("WARNING: Template name %s in BDREF is not the same as template Name found previously" % (templateName))
 				
-				if k['bdRef']['bdName'] != bdName:
+				if m['bdRef']['bdName'] != bdName:
 					print ("WARNING: BD name %s in BDREF in template %s is not the same as BD Name found previously" % (bdName,templateName))
 
-				vrf = k['vrfRef']['vrfName']
+				vrf = m['vrfRef']['vrfName']
 				schemaData[schemaID][schemaName][templateName]['vrf'].append(vrf)
 
 
-			for j in i['sites']:
-				siteName = siteData[j['siteId']]
-				siteTemplateName = j['templateName'] 
+			for n in i['sites']:
+				siteTemplateName = n['templateName'] 
+				
+				siteID = n['siteId']
+				siteName = siteData[siteID]
 
 				if siteTemplateName == templateName:
 					schemaData[schemaID][schemaName][siteTemplateName]['sites'].append(siteName)
 				else:
 					continue
 
-				if len(j['anps']) != 1:
-					print ("WARNING: %s ANP's found in schema %s, template %s IN SITE section" % (len(j['anps']), schemaName, siteTemplateName))
+				if len(n['anps']) != 1:
+					print ("WARNING: %s ANP's found in schema %s, template %s IN SITE section" % (len(n['anps']), schemaName, siteTemplateName))
 			
-				if len(j['vrfs']) != 0:
-					print ("WARNING: %s VRFs found in schema %s, template %s IN SITE section" % (len(j['vrfs']), schemaName, siteTemplateName))
-			
-				if len(j['bds']) != 1:
-					print ("WARNING: %s BDs found in schema %s, template %s IN SITE section" % (len(j['bds']), schemaName, siteTemplateName))
+				if len(n['vrfs']) != 0:
+					print ("WARNING: %s VRFs found in schema %s, template %s IN SITE section" % (len(n['vrfs']), schemaName, siteTemplateName))
+		
 
-				for k in j['anps']:
+				for o in n['anps']:
 
-					if k['anpRef']['schemaId'] != schemaID:
+					if o['anpRef']['schemaId'] != schemaID:
 						print ("WARNING: Schema ID for schema %s in SITE ANPREF of template %s not the same as schemaID found previously" % (schemaName,siteTemplateName))
 				
-					if k['anpRef']['templateName'] != siteTemplateName:
+					if o['anpRef']['templateName'] != siteTemplateName:
 						print ("WARNING: Template name %s in SITE ANPREF is not the same as template Name found previously" % (siteTemplateName))
 				
-					if k['anpRef']['anpName'] != anpName:
+					if o['anpRef']['anpName'] != anpName:
 						print ("WARNING: ANP name %s in SITE ANPREF in template %s is not the same as ANP Name found previously" % (anpName,siteTemplateName))
 				
-					for l in k['epgs']:
+					for p in k['epgs']:
 
-						if l['epgRef']['schemaId'] != schemaID:
+						if p['epgRef']['schemaId'] != schemaID:
 							print ("WARNING: Schema ID for schema %s in SITE EPGREF of template %s not the same as schemaID found previously" % (schemaName,siteTemplateName))
 				
-						if l['epgRef']['templateName'] != siteTemplateName:
+						if p['epgRef']['templateName'] != siteTemplateName:
 							print ("WARNING: Template name %s in SITE EPGREF is not the same as template Name found previously" % (siteTemplateName))
 					
-						if l['epgRef']['anpName'] != anpName:
+						if p['epgRef']['anpName'] != anpName:
 							print ("WARNING: ANP name %s in SITE EPGREF is not the same as ANP Name found previously" % (anpName))
 					
-						if l['epgRef']['epgName'] != epgName:
+						if p['epgRef']['epgName'] != epgName:
 							print ("WARNING: EPG name %s in SITE EPGREF is not the same as EPG Name found previously" % (epgName))
-			
-				for k in j['bds']:
+		
+				if len(n['bds']) == 1:
+	
+					for q in n['bds']:
+						if q['bdRef']['schemaId'] != schemaID:
+							print ("WARNING: Schema ID for schema %s in SITE BDREF of template %s not the same as schemaID found previously" % (schemaName,siteTemplateName))
 
-					if k['bdRef']['schemaId'] != schemaID:
-						print ("WARNING: Schema ID for schema %s in SITE BDREF of template %s not the same as schemaID found previously" % (schemaName,siteTemplateName))
-
-					if k['bdRef']['templateName'] != siteTemplateName:
-						# have to circle back on this one - why multiple BDs at site level
-						continue
-						print ("WARNING: Template name %s in SITE BDREF is not the same as template Name found previously" % (siteTemplateName))
+						if q['bdRef']['templateName'] != siteTemplateName:
+							# Looks like at the site level, BDs for other templates are showing up in the same template.  ex. schema SVC-COM-DCX-DC2-01 
+							print ("WARNING: Template name %s in SITE BDREF is not the same as template Name found previously" % (siteTemplateName))
 				
-					if k['bdRef']['bdName'] != bdName:
-						print ("WARNING: BD name %s in SITE BDREF in template %s is not the same as BD Name found previously" % (bdName,siteTemplateName))
+						if q['bdRef']['bdName'] != bdName:
+							print ("WARNING: BD name %s in SITE BDREF in template %s is not the same as BD Name found previously" % (bdName,siteTemplateName))
 
-					for l in k['l3Outs']:
-						schemaData[schemaID][schemaName][siteTemplateName]['l3out'].append(l)
-				
+						for r in q['l3Outs']:
+							schemaData[schemaID][schemaName][siteTemplateName]['l3out'].append(r)
+
+				if len(n['bds']) != 1:
+					#print ("checking site template " + siteTemplateName)
+					#print (temp_bds)
+					for y in temp_bds:
+						if y['templateName'] == siteTemplateName and y['siteID'] == siteID:
+							found = 1
+							
+							if y['schemaId'] not in schemaData:
+								schemaData[y['schemaId']] = {}
+							if schemaName not in schemaData[y['schemaId']]:
+								schemaData[y['schemaId']][schemaName] = {}
+							if siteTemplateName not in schemaData[y['schemaId']][schemaName]:
+								schemaData[y['schemaId']][schemaName][siteTemplateName] = {}
+								schemaData[y['schemaId']][schemaName][siteTemplateName]['l3out'] = []
+							
+							if len(y['l3Out']) > 1:
+								print ("WARNING: Multiple L3outs at SITE found for Template %s, Schema %s" % (siteTemplateName,schemaName))
+								#print (y['l3Out'])
+ 
+							if len(y['l3Out']) == 1:
+								schemaData[y['schemaId']][schemaName][siteTemplateName]['l3out'].append(y['l3Out'][0])
+							
+				if found == 0 and len(n['bds']) == 0:
+					#print (temp_bds)
+					if y['schemaId'] not in schemaData:
+						schemaData[y['schemaId']] = {}
+					if schemaName not in schemaData[y['schemaId']]:
+						schemaData[y['schemaId']][schemaName] = {}
+					if siteTemplateName not in schemaData[y['schemaId']][schemaName]:
+						schemaData[y['schemaId']][schemaName][siteTemplateName] = {}
+						schemaData[y['schemaId']][schemaName][siteTemplateName]['l3out'] = []
+							
+						schemaData[y['schemaId']][schemaName][siteTemplateName]['l3out'].append("N/A")
+
+					#print ("WARNING: Could not find l3Out for template %s, schema %s" % (siteTemplateName, schemaName))	
+				else:
+					found = 0
+					
 			
 	return schemaData
 
@@ -194,7 +284,161 @@ def main(argv):
 	siteData = loadSites('site.json')
 	tenantData = loadTenants('tenant.json')
 	schemaData = loadSchemas('schema.json',tenantData, siteData)
-	print (json.dumps(schemaData))	
+	#print (json.dumps(schemaData))	
+
+	wb = Workbook()
+	file = 'GIS_MSO_DATA.xlsx'
+	dc1_ws = wb.active
+	dc1_ws.title = 'DC1'
+
+	dc2_ws = wb.create_sheet(title='DC2')
+
+	fields = ['Schema_ID','Schema_Name','Template_Name','BD_NAME','VRF_Name','Tenant_Name','ANP_Name','site_name','EPG_Name','L3Out_Name']
+	row = 1
+	for col in range (1,11):	
+		cell = dc1_ws.cell(column=col, row=row)
+		cell.value = fields[col-1]
+	
+		cell = dc2_ws.cell(column=col, row=row)
+		cell.value = fields[col-1]
+
+	dc1_row = 2	
+	dc2_row = 2	
+	col = 1
+
+	for schemaID in schemaData:
+		for schemaName in schemaData[schemaID]:
+			for templateName in schemaData[schemaID][schemaName]:
+				if 'bd' in schemaData[schemaID][schemaName][templateName]:
+					if len(schemaData[schemaID][schemaName][templateName]['bd']) > 1:
+						print ("WARNING: More than 1 BD found in Schema %s, template %s" % (schemaName,templateName)) 
+					bdName = schemaData[schemaID][schemaName][templateName]['bd'][0]
+				else:
+					bdName = 'N/A'
+				
+				if 'vrf' in schemaData[schemaID][schemaName][templateName]:
+					if len(schemaData[schemaID][schemaName][templateName]['vrf']) > 1:
+						print ("WARNING: More than 1 VRF found in Schema %s, template %s" % (schemaName,templateName)) 
+					vrfName = schemaData[schemaID][schemaName][templateName]['vrf'][0]
+				else:
+					vrfName = 'N/A'
+				
+				if 'tenant' in schemaData[schemaID][schemaName][templateName]:
+					if len(schemaData[schemaID][schemaName][templateName]['tenant']) > 1:
+						print ("WARNING: More than 1 Tenant found in Schema %s, template %s" % (schemaName,templateName)) 
+					tenantName = schemaData[schemaID][schemaName][templateName]['tenant'][0]
+				else:
+					tenantName = 'N/A'
+				
+				if 'anp' in schemaData[schemaID][schemaName][templateName]:
+					if len(schemaData[schemaID][schemaName][templateName]['anp']) > 1:
+						print ("WARNING: More than 1 ANP found in Schema %s, template %s" % (schemaName,templateName)) 
+					anpName = schemaData[schemaID][schemaName][templateName]['anp'][0]
+				else:
+					anpName = 'N/A'
+				
+				if 'sites' in schemaData[schemaID][schemaName][templateName]:
+					if len(schemaData[schemaID][schemaName][templateName]['sites']) > 1:
+						print ("WARNING: More than 1 Site found in Schema %s, template %s" % (schemaName,templateName)) 
+					siteName = schemaData[schemaID][schemaName][templateName]['sites'][0]
+				else:
+					siteName = 'N/A'
+				
+				if 'epgs' in schemaData[schemaID][schemaName][templateName]:
+					if len(schemaData[schemaID][schemaName][templateName]['epgs']) > 1:
+						print ("WARNING: More than 1 EPG found in Schema %s, template %s" % (schemaName,templateName)) 
+					epgName = schemaData[schemaID][schemaName][templateName]['epgs'][0]
+				else:
+					epgName = 'N/A'
+				
+				if 'l3out' in schemaData[schemaID][schemaName][templateName]:
+					if len(schemaData[schemaID][schemaName][templateName]['l3out']) > 1:
+						print ("WARNING: More than 1 L3Out found in Schema %s, template %s" % (schemaName,templateName)) 
+						print (schemaData[schemaID][schemaName][templateName]['l3out'])
+					if len(schemaData[schemaID][schemaName][templateName]['l3out']) == 0:
+						l3outName = 'N/A'
+					else:
+						l3outName = schemaData[schemaID][schemaName][templateName]['l3out'][0]
+				else:
+					l3outName = 'N/A'
+
+				if bool(re.search('DC1',siteName,re.IGNORECASE)):
+					cell = dc1_ws.cell(column=1, row=dc1_row)
+					cell.value = schemaID
+
+					cell = dc1_ws.cell(column=2, row=dc1_row)
+					cell.value = schemaName
+					
+					cell = dc1_ws.cell(column=3, row=dc1_row)
+					cell.value = templateName
+					
+					cell = dc1_ws.cell(column=4, row=dc1_row)
+					cell.value = bdName
+					
+					cell = dc1_ws.cell(column=5, row=dc1_row)
+					cell.value = vrfName
+					
+					cell = dc1_ws.cell(column=6, row=dc1_row)
+					cell.value = tenantName
+					
+					cell = dc1_ws.cell(column=7, row=dc1_row)
+					cell.value = anpName
+					
+					cell = dc1_ws.cell(column=8, row=dc1_row)
+					cell.value = siteName
+					
+					cell = dc1_ws.cell(column=9, row=dc1_row)
+					cell.value = epgName
+					
+					if l3outName == 'N/A':
+						l3outName = reconcileL3out('DC1',bdName,tenantName)
+					
+					cell = dc1_ws.cell(column=10, row=dc1_row)
+					cell.value = l3outName
+
+
+					dc1_row = dc1_row + 1
+
+				else:
+					cell = dc2_ws.cell(column=1, row=dc2_row)
+					cell.value = schemaID
+
+					cell = dc2_ws.cell(column=2, row=dc2_row)
+					cell.value = schemaName
+					
+					cell = dc2_ws.cell(column=3, row=dc2_row)
+					cell.value = templateName
+					
+					cell = dc2_ws.cell(column=4, row=dc2_row)
+					cell.value = bdName
+					
+					cell = dc2_ws.cell(column=5, row=dc2_row)
+					cell.value = vrfName
+					
+					cell = dc2_ws.cell(column=6, row=dc2_row)
+					cell.value = tenantName
+					
+					cell = dc2_ws.cell(column=7, row=dc2_row)
+					cell.value = anpName
+					
+					cell = dc2_ws.cell(column=8, row=dc2_row)
+					cell.value = siteName
+					
+					cell = dc2_ws.cell(column=9, row=dc2_row)
+					cell.value = epgName
+					
+					if l3outName == 'N/A':
+						l3outName = reconcileL3out('DC2',bdName,tenantName)
+
+					cell = dc2_ws.cell(column=10, row=dc2_row)
+					cell.value = l3outName
+					
+
+					dc2_row = dc2_row + 1
+							
+	print ("Excel file saved to name " + file)
+	print ("Load ACI config and check L3Out")
+	wb.save(filename = file)
 
 if __name__ == '__main__':
 	main(sys.argv[1:])
